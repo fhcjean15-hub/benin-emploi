0f857f1e55a2bb07746e60b5f4de8038f90867ef
programmeur06 <groupesessinoubusiness@gmail.com>
2026-01-17 23:10:35 +0100

    second add

--- app/Http/Resources/BlogCollection.php
+++ app/Http/Resources/BlogCollection.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class BesoinCollection extends ResourceCollection
+class BlogCollection extends ResourceCollection
 {
     public function toArray(Request $request): array
     {
